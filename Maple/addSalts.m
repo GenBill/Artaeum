b07e6cd3fd59fca97ec9b84019f27f851956f797
Bill Zhang <30310636+GenBill@users.noreply.github.com>
2021-03-24 11:21:36 +0800

    Add files via upload

---
+++ Maple/addSalts.m
@@ -0,0 +1,15 @@
+function O_Img = addSalts(I_Img)
+% add some Salts
+% type: 
+%   0 add salts
+%   1 add salty curve
+%   2 gaussian blur
+%   3 random twist
+%   4 color inverse
+% randomly add different types noises
+if (type==0)
+    
+
+end
+
+end
