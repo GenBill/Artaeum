884a19be1a815e36476814d1fdffd9e505c2d6be
Bill Zhang <zhang478127826@gmail.com>
2021-04-09 17:47:35 +0800

    NiasGit - 2021-04-09T09:47:35.744Z

--- Maple/Bad_main.m
+++ Maple/Bad_main.m
@@ -5,6 +5,9 @@ ii = 0;
 
 Real_read = imread(['F:\Matlab\Maple\RealImg\RealImg_',num2str(ii),'.bmp']);
 Ref_read = imread(['F:\Matlab\Maple\RefImg\RefImg_',num2str(ii),'.bmp']);
+
+% Real_read = imread(['/home/genbill/Artaeum/Maple/RealImg/RealImg_',num2str(ii),'.bmp']);
+% Ref_read = imread(['/home/genbill/Artaeum/Maple/RefImg/RefImg_',num2str(ii),'.bmp']);
 Real_bedge = edge(Medi_filter(Real_read, 3),'canny');
 Ref_bedge = edge(Ref_read,'canny');
 
@@ -22,7 +25,7 @@ subplot(2,2,2); imshow(Temp)
 %% 计算中心坐标
 % [Real_Lx, Real_Ly] = size(Real_read)
 % [Ref_Lx, Ref_Ly] = size(Ref_read)
-[Ret_X, Ret_Y] = Xor_filter(Real_bedge, Ref_bedge);
+% [Ret_X, Ret_Y] = Xor_filter(Real_bedge, Ref_bedge);
 
 
 
